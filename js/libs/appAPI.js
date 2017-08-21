df2af666327091aacbe7f2d6c135bfc4642ceb47
João Victor Magela <joao.dk16@gmail.com>
2017-08-21 04:32:56 -0300

    identificar se o título é um objeto ou string

--- js/libs/appAPI.js
+++ js/libs/appAPI.js
@@ -704,8 +704,10 @@ ActionAPI.SpeechAction.prototype.ExecuteAppAction = function(action){
     }
 };
 
-ActionAPI.SpeechAction.prototype.RegisterTitle = function(title, abstractName, $context) {
+ActionAPI.SpeechAction.prototype.RegisterTitle = function(param, abstractName, $context) {
     var $data, $bind, $dataObj, $env;
+
+    var tltle = _.isObject(param) ? param[this.currentLanguage] : param; 
     
     if($context != null){
         $data = $context.$data; $bind = $context.$bind; $dataObj = $context.$dataObj;  $env = $context.$env;
