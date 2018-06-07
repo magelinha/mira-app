477618452f12c2c05225eaebcc59d5cce0a80f28
João Victor Magela <joao.dk16@gmail.com>
2018-06-07 01:20:59 -0300

    atualizaçao do fastfood

--- js/example/fastfood.js
+++ js/example/fastfood.js
@@ -386,7 +386,7 @@ var landingConcreta =
         },
 
         //Corpo
-        { name: 'itens', tag: 'tbody', widget: 'WaiList' },
+        { name: 'itens', tag: 'tbody', widget: 'WaiListContent' },
         { 
             name: 'item', 
             tag: 'tr', 
