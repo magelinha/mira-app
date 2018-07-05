d59405eb3f5f7fa027c68ef2ee9bb1ce31298c16
magela <joao.dk16@gmail.com>
2018-07-05 11:48:14 -0300

    atualização do fastfood

--- js/example/fastfood.js
+++ js/example/fastfood.js
@@ -350,6 +350,7 @@ var landingConcreta =
             class: "form-horizontal", 
             events:{ 
                 submit: {
+                    action: 'EvtSubmitForm',
                     event: 'item_adicionado', 
                     params: { 
                         item: '$("#cardapio option:selected").text()', 
@@ -923,6 +924,16 @@ if(typeof define === 'function') {
                     return;
 
                 window.location.href = options.$element.prop('href');
+            }
+
+            window.EvtSubmitForm = function(options){
+                //Evita que o submit seja executado
+                options.$event.preventDefault();
+                
+                setTimeout(function(){
+                    //depois de 1 segundo, alerta ao MIRA que a coleção foi modificada
+                    app.$env.$dataObj.trigger("change"); 
+                }, 1000);
             }
 
             //#endregion
