7ee37fd168a7dd6db6fde473691934a074bd3b15
João Victor Magela <joao.dk16@gmail.com>
2018-07-11 03:47:46 -0300

    fix error

--- js/mira/dialogflow/webhook.js
+++ js/mira/dialogflow/webhook.js
@@ -518,6 +518,7 @@ var Init = function(server){
 		var total = 0; 
 		pedidos.itens.forEach(item => total += item.total);
 
+		console.log(total);
 		var totalMoeda = formatPrice(total);
 
 		return `O total do pedido é ${totalMoeda}`;
