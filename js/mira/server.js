9bd71b20fb505ec1c22c6c34dd52ebc47b179a92
João Victor Magela <joao.dk16@gmail.com>
2018-11-27 02:18:24 -0200

    remoção do storage

--- js/mira/server.js
+++ js/mira/server.js
@@ -30,32 +30,32 @@ server.use(
     })
 );
 
-// Imports the Google Cloud client library.
-const Storage = require('@google-cloud/storage');
+// // Imports the Google Cloud client library.
+// const Storage = require('@google-cloud/storage');
 
-// Instantiates a client. Explicitly use service account credentials by
-// specifying the private key file. All clients in google-cloud-node have this
-// helper, see https://github.com/GoogleCloudPlatform/google-cloud-node/blob/master/docs/authentication.md
-const storage = new Storage({
-    projectId: 'booking-cf175',
-    //keyFilename: './js/mira/dialogflow/keys/fastfood.json'
-});
+// // Instantiates a client. Explicitly use service account credentials by
+// // specifying the private key file. All clients in google-cloud-node have this
+// // helper, see https://github.com/GoogleCloudPlatform/google-cloud-node/blob/master/docs/authentication.md
+// const storage = new Storage({
+//     projectId: 'booking-cf175',
+//     //keyFilename: './js/mira/dialogflow/keys/fastfood.json'
+// });
 
-// Makes an authenticated API request.
+// // Makes an authenticated API request.
 
-storage
-  .getBuckets()
-  .then((results) => {
-    const buckets = results[0];
+// storage
+//   .getBuckets()
+//   .then((results) => {
+//     const buckets = results[0];
 
-    console.log('Buckets:');
-    buckets.forEach((bucket) => {
-      console.log(bucket.name);
-    });
-  })
-  .catch((err) => {
-    console.error('ERROR:', err);
-  });
+//     console.log('Buckets:');
+//     buckets.forEach((bucket) => {
+//       console.log(bucket.name);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error('ERROR:', err);
+//   });
 
 
 // criando servidor para arquivos estaticos
