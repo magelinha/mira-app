aad4fb8a38675fb8c377b82e009d1b3c8fd8eee6
João Victor Magela <joao.dk16@gmail.com>
2019-02-26 03:25:14 -0300

    Update fastfoodnovo.js

--- js/example/fastfoodnovo.js
+++ js/example/fastfoodnovo.js
@@ -268,6 +268,10 @@ var cardapioConcreta =
                 },
             ] 
         },
+
+        //preço
+        { name: "item-preco", widget: "WaiContent", class:"content-item" },
+        { name: "preco", widget: "WaiContent", tag: "p", value:"$dataObj.preco", when: "_.isNumber($dataObj.preco)" }
     ]
 };
 
