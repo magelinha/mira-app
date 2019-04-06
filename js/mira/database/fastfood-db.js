aba677f049454dca03e84bd69fe9c17d61bbdf38
João Victor Magela <joao.dk16@gmail.com>
2019-04-05 22:23:52 -0300

    instalação do mongoose

---
+++ js/mira/database/fastfood-db.js
@@ -0,0 +1,39 @@
+"use strict";
+
+//Constantes para acesso ao banco de dados
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017/magela-db";
+const mongoose = require('monogoose');
+var db_heroku = 'heroku_2hlrrqz9';
+
+//Models para o fastfood
+const Item = mongoose.model('Item', new mongoose.Schema({
+    _id: mongoose.Schema.Types.Number,
+    nome: mongoose.Schema.Types.String,
+    imagem: mongoose.Schema.Types.String,
+    preco: mongoose.Schema.Types.Mixed,
+    descricao: mongoose.Schema.Types.String
+}));
+
+const Categoria = mongoose.model('Categoria', new mongoose.Schema({
+    _id: mongoose.Schema.Types.Number,
+    nome: mongoose.Schema.Types.String,
+    itens: [{ type: mongoose.Schema.Types.Number, ref: 'Item'}]
+}));
+
+const Pedido = moongose.model('Pedido', new mongoose.Schema({
+    _id: mongoose.Schema.Types.Number,
+    numero: mongoose.Schema.Types.String,
+    email: mongoose.Schema.Types.String,
+    item: { type: mongoose.Schema.Types.ObjectId, ref:'Item' }
+}));
+
+var Init = function(){
+    mongoose.connect(url, { useNewUrlParser: true});
+}
+
+module.exports = {
+    Init,
+    Item,
+    Categoria,
+    Pedido
+};
---
+++ js/mira/examples-modules/fastfoodnovo.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const db = require('../database/fastfood-db.js');
+
+var Init = function(server){
+    db.Init();
+
+    //Para cada link, gera o resultado desejado
+};
+
+module.exports = {
+    Init,
+}
